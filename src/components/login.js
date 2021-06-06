632827e6492d7dfe7987f3efa8112beb94ee0846
Ajay Yakkateela <ajay.y@kuriosity-labs.com>
2021-06-06 16:12:04 +0530

    aj-shopping

---
+++ src/components/login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class login extends Component {
+    render() {
+        return (
+            <div>
+                welcome to AJ-SHOPPING
+            </div>
+        );
+    }
+}
+
+export default login;
